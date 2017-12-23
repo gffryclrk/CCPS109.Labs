f3cde7f0c0e7a7d32261830f313d04f16da6df98
gffryclrk <mr@gffryclrk.com>
2017-12-23 12:50:20 -0500

    Lab 04 Tests Passed

--- StringProblems.java
+++ StringProblems.java
@@ -7,7 +7,8 @@
  */
 public class StringProblems
 {
-    public String removeDuplicates(String s){
+    public String removeDuplicates_original(String s){
+        // I misread the question but this code does what I intended (I think)
         StringBuilder sUnique = new StringBuilder("");
         //sUnique = "";
         for(int i=0;i<s.length();i+=1){
@@ -18,6 +19,16 @@ public class StringProblems
             if(cUnique){ sUnique.append(s.charAt(i)); }//sUnique = sUnique + s.charAt(i); }
         }
         return sUnique.toString();
+    }
+    public String removeDuplicates(String s){
+    	StringBuilder sUnique = new StringBuilder("");
+    	if(s.length() > 0){
+	    	sUnique.append(s.charAt(0));
+	    	for(int i=1; i<s.length(); i+=1){
+	    		if(sUnique.charAt(sUnique.length()-1) != s.charAt(i)) sUnique.append(s.charAt(i));
+	    	}
+	    }
+    	return sUnique.toString();
     }
     public int countWords(String s){
     	int count = 0;
@@ -54,23 +65,34 @@ public class StringProblems
     }
     public String stretch(String s, int n){
     	StringBuilder stretchString = new StringBuilder("");
-    	if(n==0 || n > 100){
-    		// stretchString = s.charAt(0) + s.charAt(s.length());
-    		// stretchString = s.substring(0,1) + s.substring(s.length(), 1);
-    		//stretchString.appe
+    	if(s.length() == 0){
+
+    	}else if(s.length() == 1){
+    			stretchString.insert(0, s.charAt(0));
     	}else{
-    		for(int i=1; i<s.length()-1; i+=1 ){
-    			for(int j=0; j<n; j+=1){
-    				// stretchString += s.charAt(i);
-    				stretchString.append(s.charAt(i));
-    			}
-    		}
-    		// stretchString.insert(0, s.charAt(0));
-    		// stretchString.append(s.charAt(s.length()));
-    		// stretchString.insert(0, s.substring(0,1)).append(s.substring(s.length(), 1));
-    	}
-    	// stretchString.insert(0, s.substring(0,1)).append(s.charAt(s.length()-1));
-    	stretchString.insert(0, s.charAt(0)).append(s.charAt(s.length()-1));
+    		
+	    	if(n==0 || n > 100){
+	    		// stretchString = s.charAt(0) + s.charAt(s.length());
+	    		// stretchString = s.substring(0,1) + s.substring(s.length(), 1);
+	    		//stretchString.appe
+
+	    		
+	    	}else{
+	    		for(int i=1; i<s.length()-1; i+=1 ){
+	    			for(int j=0; j<n; j+=1){
+	    				// stretchString += s.charAt(i);
+	    				stretchString.append(s.charAt(i));
+	    			}
+	    		}
+	    		// stretchString.insert(0, s.charAt(0));
+	    		// stretchString.append(s.charAt(s.length()));
+	    		// stretchString.insert(0, s.substring(0,1)).append(s.substring(s.length(), 1));
+	    		// stretchString.insert(0, s.charAt(0)).append(s.charAt(s.length()-1));
+	    	}
+	    	// stretchString.insert(0, s.substring(0,1)).append(s.charAt(s.length()-1));
+	    	// stretchString.insert(0, s.charAt(0)).append(s.charAt(s.length()-1));
+	    	stretchString.insert(0, s.charAt(0)).append(s.charAt(s.length()-1));
+	    }
     	return stretchString.toString();
 
     }
