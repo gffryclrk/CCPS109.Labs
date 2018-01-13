e9ba71479699fd24baa94077c3995494a92e68dd
gffryclrk <mr@gffryclrk.com>
2018-01-13 10:48:48 -0500

    Lab 09 5/6

--- RecursionProblems.java
+++ RecursionProblems.java
@@ -5,7 +5,7 @@
  * @author Geoffrey Clark
  * @version 0.1
  */
-// import java.util.Arrays;
+import java.util.Arrays;
 public class RecursionProblems
 {
     public boolean allEqual(int[] arr, int start, int end){
@@ -19,6 +19,32 @@ public class RecursionProblems
             tgt[start2] = src[start];
             // System.out.println(Arrays.toString(tgt));
             arraycopy(src, start+1, tgt, start2+1, len-1);
+        }
+    }
+    boolean linearSearch(int[] arr, int x, int start, int end){
+        if(arr[start] == x) return true;
+        else if(start == end) return false;
+        else return linearSearch(arr, x, start+1, end);
+    }
+    public void parityPartition(int[] arr, int start, int end){
+        if(arr.length > 1 && start <= end){
+            int[] arrOut = new int[arr.length]; // This is a new array to hold my mixed up copies...
+            if((arr[start] & 1)==0){                
+                // First copy element to the end
+                // However there's a trick: If start > 0 I need to do to copies: Before & after the position. 
+                // I'm finding myself genuinely surprised that there isn't an easier way to do this (perhaps there is)
+                System.arraycopy(arr, start, arrOut, arr.length-1,1);
+                // Next, copy rest of array:
+                // First: before start
+                System.arraycopy(arr, 0, arrOut, 0, start);
+                // Second: After start
+                System.arraycopy(arr, start+1, arrOut, start, arr.length-1-start);
+                // Now copy everything back to the original array and pass it to itself. 
+                System.arraycopy(arrOut, 0, arr, 0, arr.length);
+                parityPartition(arr, start, end-1);
+            }else{
+                parityPartition(arr, start+1, end);
+            }
         }
     }
 }
