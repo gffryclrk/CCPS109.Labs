91f48dfce72449fdeb0834a1a3aafb43b0286f43
gffryclrk <mr@gffryclrk.com>
2018-01-19 09:58:13 -0500

    Lab 10 Alternatives: 3/4 Tests Pass

--- ChessProblems.java
+++ ChessProblems.java
@@ -10,6 +10,9 @@ public class ChessProblems
     private static boolean inside(int i, int j, int n) {
         return 0 <= i && i < n && 0 <= j && j < n;
     }
+    private static int[][] knightMoves = {
+        {2,1},{1,2},{-1,2},{-2,1},{-2,-1},{-1,-2},{1,-2},{2,-1}
+    };
     public int countSafeSquaresRooks_01(int n, boolean[][] rooks){
         int count = 0;
         boolean[] row = new boolean[rooks.length];
@@ -88,8 +91,46 @@ public class ChessProblems
         }
         return (n*n) - count;
     }
+    public int countKnightMoves1(int n, boolean[][] knights){
+        /* This method doesn't work */
+        /* I was incorrectly counting safe squares; analagous to previous 2 methods. */
+        /* Counting number of moves is another issue. */
+
+        // {2,1},{1,2},{-1,2},{-2,1},{-2,-1},{-1,-2},{1,-2},{2,-1}
+        boolean[][] moves = new boolean[knights.length][knights[0].length];
+        int count = 0;        
+        for(int i=0; i<(n*n); i+=1){ //For every square in supplied chessboard
+            if(knights[(i/n)][(i%n)]){ // if there's a knight on the square
+                for(int j=0; j<(n*n); j+=1){ // go through every square on possible moves board
+                    for(int k=0; k<knightMoves.length; k+=1){ // loop through possible night moves
+                        if(inside((i/n)+knightMoves[k][0], (i%n)+knightMoves[k][1], n)) //if the possible move is inside the board
+                            //System.out.println("TRUE!");
+                            if(!moves[((i/n)+knightMoves[k][0])][((i%n)+knightMoves[k][1])] && !knights[((i/n)+knightMoves[k][0])][((i%n)+knightMoves[k][1])]){
+                                moves[((i/n)+knightMoves[k][0])][((i%n)+knightMoves[k][1])] = true;
+                                count += 2; //It seems as if he considers moving to the same square different ways as different moves (I'm not sure how I feel about this)
+                            }
+                    }
+                }
+            }
+        }
+        // System.out.println(java.util.Arrays.deepToString(knights));
+        // System.out.println(count);
+        return count;
+    }
     public int countKnightMoves(int n, boolean[][] knights){
-        return 0;
+        int moves = 0;
+        for(int i=0; i<(n*n); i+=1){
+            if(knights[(i/n)][(i%n)]){
+                for(int k=0; k<knightMoves.length; k+=1){
+                    if(inside((i/n)+knightMoves[k][0], (i%n)+knightMoves[k][1], n)){
+                        if(!knights[((i/n)+knightMoves[k][0])][((i%n)+knightMoves[k][1])]){
+                            moves += 1;
+                        }
+                    }
+                }
+            }
+        }
+        return moves;
     }
     public int countPawnMoves(int n, char[][] board){
         return 0;
